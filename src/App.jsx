d1bd7068c5e26a16c02c32a4e0f71e4506eec29c
alexeimdev <alexeim.dev@gmail.com>
2023-12-26 14:20:26 +0200

    react

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div class="wrapper">
+      <div class="container">
+        <header class="header">
+          <p>Header</p>
+        </header>
+        <main class="main">
+          <p>Main first</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main</p>
+          <p>Main last</p>
+        </main>
+        <footer class="footer">
+          <p>Footer disabled</p>
+          <p>Footer enabled</p>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default App
