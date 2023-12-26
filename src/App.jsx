cf0e75b7c8c3c2d552ce0921569f6f192ca95eb2
alexeimdev <alexeim.dev@gmail.com>
2023-12-26 15:33:23 +0200

    logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import questions from './data/questions.json'
 
 function App() {
+  const [radioValue, setRadioValue] = useState();
+  const handleRadioChange = (e) => {
+    setRadioValue(e.target.value);
+  }
+
   return (
-    <div class="wrapper">
-      <div class="container">
-        <header class="header">
+    <div className="wrapper">
+      <div className="container">
+        <header className="header">
           <p>Header</p>
         </header>
-        <main class="main">
-          <p>Main first</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main</p>
-          <p>Main last</p>
+        <main className="main">
+          <div>
+            <label>der<input type='radio' hidden name="article" value="der" onChange={handleRadioChange}></input></label>
+            <label>die<input type='radio' hidden name="article" value="die" onChange={handleRadioChange}></input></label>
+            <label>das<input type='radio' hidden name="article" value="das" onChange={handleRadioChange}></input></label>
+          </div>
+          <br/>
+          <br/>
+          <div>
+            <span>{questions[1].word}</span>
+          </div>
         </main>
-        <footer class="footer">
-          <p>Footer disabled</p>
-          <p>Footer enabled</p>
+        <footer className="footer">
+          {radioValue != null && (radioValue === questions[1].article ? "Corrent V" : "Wrong X")}
         </footer>
       </div>
     </div>
